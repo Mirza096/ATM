c2526830a47546a83783b5a604f003a658010f16
Mirza096 <mirza096@gmail.com>
2024-03-27 23:43:59 +0500

    entire code change, with the totally new pattern

--- main.ts
+++ main.ts
@@ -1,57 +1,75 @@
 import inquirer from "inquirer"
 
-let myBalance = 10000; //Dollar
-let myPin = 1234;
+interface UserAnswers{
 
-let pinAnswer = await inquirer.prompt(
-    [
-        {   name: "pin",
-            message: "enter your pin",
-            type: "number"
+    userID : string;
+    userPIN : number;
+    accountType: string;
+    transactionType: string;
+    amount: number;
+
 }
-]
-);
-if (pinAnswer.pin === myPin ){
-    console.log("correct pin code!!!");
 
-    let operationAns = await inquirer.prompt(
-        [
-            {
-                name: "operation",
-                message: "please select options",
-                type: "list",
-                choices: ["withdraw", "balance check", "cancel"]
-            }
-        ]
-    );
-    
-        let amountAns = await inquirer.prompt(
-            
-            [
-                {
-                    name: "amount",
-                    message: "enter your amount",
-                    type: "number"
-                }
-            ]
-        );
-        // withdraw block
-    if(operationAns.operation === "withdraw" && operationAns.operation <= myBalance){
-        // =, -=, +=
-        console.log(operationAns);
-        myBalance -= amountAns.amount;
-        console.log("your remaining balance is: "+ myBalance);
+async function StartATMConversation() {
+    console.log("Welcome To Bank Al-Habib Limited")
 
-    } else if(operationAns.operation === "balance check"){
-        console.log(myBalance); 
 
-    } else if(operationAns.operation > myBalance){
-        console.log("you have insufficient balance");
-        
-    } else if(operationAns.operation === "cancel"){
-        console.log("take out your card");
+const   answers: UserAnswers = await inquirer.prompt([
+    {
+        type: "input",
+        name: "userID",
+        message: "Kindly enter your user ID",
+    },
+    {
+        type: "number",
+        name: "userPIN",
+        message: "Kindly enter your PIN Code",
+    },
+    {
+        type: "list",
+        name: "accountType",
+        choices: ["Current", "Saving"],
+        message: "Select your accountType",
+    },
+    {
+        type: "list",
+        name: "transactionType",
+        choices: ["Fastcash", "Normal Withdrawal"],
+        message: "Select your transactionType",
+        when(answers){
+           return answers.accountType;
+        }
+    },
+    {
+        type: "list",
+        name: "amount",
+        choices: [1000, 2000, 5000, 10000, 15000, 20000],
+        message: "Select your Cash amount",
+        when(answers){
+            return answers.transactionType === "Fastcash";
+        }
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter your amount",
+        when(answers){
+            return answers.transactionType === "Normal Withdrawal";
+        }
+    },
+])
+if(answers.userID && answers.userPIN){
+    console.log("Processing your request....");
+    const balance = Math.floor(Math.random()* 10000000);
+    console.log("Your current balance is: PKR", balance.toLocaleString());
+    const enteredAmount = answers.amount;
+
+    if(balance >= enteredAmount){
+        const remainingBalance = balance - enteredAmount;
+        console.log("Transaction is successful. Your remaining balance is PKR", remainingBalance.toLocaleString());
+    } else{
+        console.log("Insufficient balance. Please try again");
     }
-else {
-    console.log("incorrect pin numbber");
 }
 }
+StartATMConversation();
